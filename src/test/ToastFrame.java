47b14ab91d3f2ea3e00bbe9a20e0069c76da11a8
mocchi <boom.boom.planet@gmail.com>
2015-01-24 22:54:46 +0900

    FIX: while cond

--- src/test/ToastFrame.java
+++ src/test/ToastFrame.java
@@ -10,20 +10,23 @@ public class ToastFrame {
     public static void main(String[] args) {
         int i = 0;
         String[] face = {"J(>_<)l","J(^-^)l", "J(^o^)l", "J(^o^)/"}; 
+        String[] mes = {"WakeUp!","WakeUp!!","WakeUp!!!"};
         frame.ToastFrame frame = new frame.ToastFrame(WindowEnvironment.RIGHT,WindowEnvironment.TOP);
         frame.setMouseListener(new DefaultMouseListener());
-        JLabel iconJLabel = new JLabel("J(^-^)l");
-        frame.setText("WakeUp!!!");
+        JLabel iconJLabel = new JLabel(face[face.length - 1]);
+        JLabel textJLabel = new JLabel(mes[mes.length - 1]);
+        frame.setText(textJLabel);
         frame.setIcon(iconJLabel);
         frame.reshow();
         frame.setVisible(true);
-        while (frame.isActive()) {
+        while (frame.isVisible()) {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+            textJLabel.setText(mes[Math.abs(i%mes.length)]);
             iconJLabel.setText(face[Math.abs(i++%face.length)]);
         }
     }
